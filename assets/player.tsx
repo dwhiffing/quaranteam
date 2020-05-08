c99002cb3fad636fe0186ff2451bbaad822eb08f
Daniel Whiffing <danielwhiffing@gmail.com>
2020-05-08 19:22:46 -0400

    Updates

---
+++ assets/player.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="player" tilewidth="70" tileheight="70" tilecount="20" columns="5">
+ <image source="player.png" width="358" height="293"/>
+</tileset>
